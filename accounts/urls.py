afe191e2fd0c8921be400a9f850344b30cc7cf78
Jon Fiorelli <jonmariofiorelli@yahoo.com>
2021-11-03 08:30:31 -0400

    Add reminder to accounts.urls of routes included by Django through django.contrib.auth.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,16 @@ from .views import SignUpView
 
 
 urlpatterns = [
+    # The following routes are included via Django provided django.contrib.auth.urls: 
+    #   path('login/', views.LoginView.as_view(), name='login'),
+    #   path('logout/', views.LogoutView.as_view(), name='logout'),
+    #   path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    #   path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    #   path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    #   path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    #   path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    #   path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),    
+
+    # Added Routes
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
