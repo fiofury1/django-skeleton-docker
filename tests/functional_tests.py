d66946475c5649e0cecc563335e615885411e244
Jon Fiorelli <jonmariofiorelli@yahoo.com>
2021-11-11 07:01:10 -0500

    Comment out test of Selenium setup.

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,14 +1,14 @@
 # Replace this file with project wide functional/integration tests
 
 # Test Selenium setup
-import time
+# import time
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
 
-service = Service('/home/fiofury1/drivers/chromedriver/chromedriver') #If project is cloned, replace with local location. 
-service.start()
-driver = webdriver.Remote(service.service_url)
-driver.get('http://www.google.com/');
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+# service = Service('/home/fiofury1/drivers/chromedriver/chromedriver') #If project is cloned, replace with local location. 
+# service.start()
+# driver = webdriver.Remote(service.service_url)
+# driver.get('http://www.google.com/');
+# time.sleep(5) # Let the user actually see something!
+# driver.quit()
