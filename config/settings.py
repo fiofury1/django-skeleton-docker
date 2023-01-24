d4cfc9b12fa994f2d85d08bffb2e99807bfad87f
Jon Fiorelli <jonmariofiorelli@yahoo.com>
2023-01-24 14:13:43 -0500

    Add security variables and values to settings.py and EXAMPLE.env.

--- config/settings.py
+++ config/settings.py
@@ -224,12 +224,12 @@ LOGOUT_REDIRECT_URL = 'main:home'
 #     EMAIL_USE_TLS = bool(int(get_env_variable('EMAIL_USE_TLS')))
 
 # #For security
-# SECURE_SSL_REDIRECT = bool(int(get_env_variable('SECURE_SSL_REDIRECT')))
-# SECURE_HSTS_SECONDS = int(get_env_variable('SECURE_HSTS_SECONDS'))
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = bool(int(get_env_variable('SECURE_HSTS_INCLUDE_SUBDOMAINS')))
-# SECURE_HSTS_PRELOAD = bool(int(get_env_variable('SECURE_HSTS_PRELOAD')))
-# SESSION_COOKIE_SECURE = bool(int(get_env_variable('SESSION_COOKIE_SECURE')))
-# CSRF_COOKIE_SECURE = bool(int(get_env_variable('CSRF_COOKIE_SECURE')))
+SECURE_SSL_REDIRECT = bool(int(get_env_variable('SECURE_SSL_REDIRECT')))
+SECURE_HSTS_SECONDS = int(get_env_variable('SECURE_HSTS_SECONDS'))
+SECURE_HSTS_INCLUDE_SUBDOMAINS = bool(int(get_env_variable('SECURE_HSTS_INCLUDE_SUBDOMAINS')))
+SECURE_HSTS_PRELOAD = bool(int(get_env_variable('SECURE_HSTS_PRELOAD')))
+SESSION_COOKIE_SECURE = bool(int(get_env_variable('SESSION_COOKIE_SECURE')))
+CSRF_COOKIE_SECURE = bool(int(get_env_variable('CSRF_COOKIE_SECURE')))
 
 if DEBUG:
     # For Django Debug Toolbar
